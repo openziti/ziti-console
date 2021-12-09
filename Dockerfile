82eb4e92de4581c278406887c406e53c07cceb5d
dovholuknf <46322585+dovholuknf@users.noreply.github.com>
2021-12-09 15:50:31 -0500

    update docker for easier setup

--- Dockerfile
+++ Dockerfile
@@ -1,14 +1,25 @@
 FROM node:10.3
 
+# The in-container path for the key file to use for TLS.
+ENV ZAC_SERVER_KEY=
+# The in-container path for the cert bundle file to use for TLS.
+ENV ZAC_SERVER_CERT_CHAIN=
+# the HTTP port ZAC uses
+ENV PORT=
+# the HTTPS port ZAC uses
+ENV PORTTLS=
+
 # Create app directory
 WORKDIR /usr/src/app
 
-# Expose port
+# Expose ports
 EXPOSE 1408
+EXPOSE 8443
 
 # Copy source code to image
 COPY . .
 
 # Fetch dependencies
 RUN npm install
-CMD ["node", "/usr/src/app/server.js"]
+
+CMD ["/usr/src/app/run-zac.sh"]
---
+++ run-zac.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# this script is used to run zac within a container
+# it only exists to add the two symlinks required to run the server with TLS by reusing the certificates
+# created by the ziti-edge-controller
+echo "running ZAC"
+if [[ "${ZAC_SERVER_KEY}" != "" ]]; then
+  echo "ZAC will use this key for TLS: ${ZAC_SERVER_KEY}"
+  ln -s "${ZAC_SERVER_KEY}" /usr/src/app/server.key
+fi
+if [[ "${ZAC_SERVER_CERT_CHAIN}" != "" ]]; then
+  echo "ZAC will present this pem for TLS: ${ZAC_SERVER_CERT_CHAIN}"
+  ln -s "${ZAC_SERVER_CERT_CHAIN}" /usr/src/app/server.chain.pem
+fi
+
+node /usr/src/app/server.js
