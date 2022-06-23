4fa2d85839e48d994a848475886b9bcdedbf7f83
Jeremy Tellier <jeremy.tellier@netfoundry.io>
2022-06-23 07:15:30 -0400

    Fix Label

--- server.js
+++ server.js
@@ -49,7 +49,7 @@ if ((typeof zitiIdentityFile !== 'undefined') && (typeof zitiServiceName !== 'un
 	await ziti.init( zitiIdentityFile ).catch(( err ) => { process.exit(); }); // Authenticate ourselves onto the Ziti network using the specified identity file
 }
 
-const zacVersion = "2.3.5";
+const zacVersion = "2.3.6";
 
 var serviceUrl = "";
 var baseUrl = "";
