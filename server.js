c57085eaf807217f44016dda9068e6c7dd7e8598
Jeremy Tellier <jeremy.tellier@netfoundry.io>
2023-01-05 08:43:15 -0500

    Rev up Version

--- server.js
+++ server.js
@@ -54,7 +54,7 @@ if ((typeof zitiIdentityFile !== 'undefined') && (typeof zitiServiceName !== 'un
 	await ziti.init( zitiIdentityFile ).catch(( err ) => { process.exit(); }); // Authenticate ourselves onto the Ziti network using the specified identity file
 }
 
-const zacVersion = "2.5.1";
+const zacVersion = "2.5.2";
 
 var serviceUrl = "";
 var baseUrl = "";
