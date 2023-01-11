1a2af591a1efc3bc96bb43f34ed93e205f246b5f
Jeremy Tellier <jeremy.tellier@netfoundry.io>
2023-01-10 20:00:34 -0500

    Semi-Fixed Suggest List - Object Only or Hash Only suggest works not mixed.

--- server.js
+++ server.js
@@ -53,7 +53,7 @@ if ((typeof zitiIdentityFile !== 'undefined') && (typeof zitiServiceName !== 'un
 	await ziti.init( zitiIdentityFile ).catch(( err ) => { process.exit(); }); // Authenticate ourselves onto the Ziti network using the specified identity file
 }
 
-const zacVersion = "2.5.3";
+const zacVersion = "2.5.4";
 
 var serviceUrl = "";
 var baseUrl = "";
