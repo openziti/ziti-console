bcef40c69520d7211fb8875e28100df5c9762597
Kenneth Bingham <kenneth.bingham@netfoundry.io>
2024-04-19 16:33:36 -0400

    break the build

--- projects/app-ziti-console/src/main.ts
+++ projects/app-ziti-console/src/main.ts
@@ -1,7 +1,7 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
-
+import NoExist from './app/no-exist';
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
