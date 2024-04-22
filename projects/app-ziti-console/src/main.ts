ec874e6a5d69db17db1bb1d3e84b403c9098a5e4
Ryan Galletto <ryan.galletto@netfoundry.io>
2024-04-22 11:53:53 -0400

    Fix breaking change for PR build check example

--- projects/app-ziti-console/src/main.ts
+++ projects/app-ziti-console/src/main.ts
@@ -1,7 +1,6 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
-import NoExist from './app/no-exist';
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
