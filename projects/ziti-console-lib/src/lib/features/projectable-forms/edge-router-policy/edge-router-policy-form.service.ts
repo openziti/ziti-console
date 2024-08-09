df5c0a591ca05bbc6a113b2baff6e47d743eff84
Ryan Galletto <ryan.galletto@netfoundry.io>
2024-08-08 21:15:51 -0400

    Correctly show associated entities when using the #all attribute

--- projects/ziti-console-lib/src/lib/features/projectable-forms/edge-router-policy/edge-router-policy-form.service.ts
+++ projects/ziti-console-lib/src/lib/features/projectable-forms/edge-router-policy/edge-router-policy-form.service.ts
@@ -109,15 +109,24 @@ export class EdgeRouterPolicyFormService {
             this.associatedEdgeRouterNames = [...namedAttributes];
             return;
         }
-        const filters = [
-            {
-                columnId: "roleAttributes",
-                filterName: "Edge Router Attributes",
-                label: "",
-                type: "ATTRIBUTE",
-                value: roleAttributes
+        let hasAll = false;
+        roleAttributes.forEach((attr) => {
+            if (attr === 'all') {
+                hasAll = true;
             }
-        ];
+        });
+        let filters = [];
+        if (!hasAll) {
+            filters = [
+                {
+                    columnId: "roleAttributes",
+                    filterName: "Edge Router Attributes",
+                    label: "",
+                    type: "ATTRIBUTE",
+                    value: roleAttributes
+                }
+            ];
+        }
         const paging = this.zitiService.DEFAULT_PAGING;
         paging.noSearch = false;
         this.zitiService.get('edge-routers', paging, filters).then((result: any) => {
@@ -136,15 +145,24 @@ export class EdgeRouterPolicyFormService {
             this.associatedIdentityNames = [...namedAttributes];
             return;
         }
-        const filters = [
-            {
-                columnId: "roleAttributes",
-                filterName: "Identity Attributes",
-                label: "",
-                type: "ATTRIBUTE",
-                value: roleAttributes
+        let hasAll = false;
+        roleAttributes.forEach((attr) => {
+            if (attr === 'all') {
+                hasAll = true;
             }
-        ];
+        });
+        let filters = [];
+        if (!hasAll) {
+            filters = [
+                {
+                    columnId: "roleAttributes",
+                    filterName: "Identity Attributes",
+                    label: "",
+                    type: "ATTRIBUTE",
+                    value: roleAttributes
+                }
+            ];
+        }
         const paging = this.zitiService.DEFAULT_PAGING;
         paging.noSearch = false;
         this.zitiService.get('identities', paging, filters).then((result: any) => {
--- projects/ziti-console-lib/src/lib/features/projectable-forms/service-edge-router-policy/service-edge-router-policy-form.service.ts
+++ projects/ziti-console-lib/src/lib/features/projectable-forms/service-edge-router-policy/service-edge-router-policy-form.service.ts
@@ -91,15 +91,24 @@ export class ServiceEdgeRouterPolicyFormService {
             this.associatedEdgeRouterNames = [...namedAttributes];
             return;
         }
-        const filters = [
-            {
-                columnId: "roleAttributes",
-                filterName: "Edge Router Attributes",
-                label: "",
-                type: "ATTRIBUTE",
-                value: roleAttributes
+        let hasAll = false;
+        roleAttributes.forEach((attr) => {
+            if (attr === 'all') {
+                hasAll = true;
             }
-        ];
+        });
+        let filters = [];
+        if (!hasAll) {
+            filters = [
+                {
+                    columnId: "roleAttributes",
+                    filterName: "Edge Router Attributes",
+                    label: "",
+                    type: "ATTRIBUTE",
+                    value: roleAttributes
+                }
+            ];
+        }
         const paging = this.zitiService.DEFAULT_PAGING;
         paging.noSearch = false;
         this.zitiService.get('edge-routers', paging, filters).then((result: any) => {
@@ -118,15 +127,24 @@ export class ServiceEdgeRouterPolicyFormService {
             this.associatedServiceNames = [...namedAttributes];
             return;
         }
-        const filters = [
-            {
-                columnId: "roleAttributes",
-                filterName: "Service Attributes",
-                label: "",
-                type: "ATTRIBUTE",
-                value: roleAttributes
+        let hasAll = false;
+        roleAttributes.forEach((attr) => {
+            if (attr === 'all') {
+                hasAll = true;
             }
-        ];
+        });
+        let filters = [];
+        if (!hasAll) {
+            filters = [
+                {
+                    columnId: "roleAttributes",
+                    filterName: "Service Attributes",
+                    label: "",
+                    type: "ATTRIBUTE",
+                    value: roleAttributes
+                }
+            ];
+        }
         const paging = this.zitiService.DEFAULT_PAGING;
         paging.noSearch = false;
         this.zitiService.get('services', paging, filters).then((result: any) => {
