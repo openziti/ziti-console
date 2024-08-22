9e1d679bdcbc9f59c3658ff51448ce9a42e8618a
Kenneth Bingham <kenneth.bingham@netfoundry.io>
2024-08-22 13:08:31 -0400

    respond to pull request feedback

--- build.sh
+++ build.sh
@@ -13,4 +13,5 @@ fi
 npm install
 npm install -g @angular/cli@16.0.0-next.0
 ng build ziti-console-lib
-ng build ziti-console --base-href "$1"
+# WARN: deployUrl deprecated since Angular 13, pending decommission in future ng CLI
+ng build ziti-console --base-href "$1" --deploy-url "$1"
