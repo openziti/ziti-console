3f6e72dbdae983113b7b1dbc0280f35ba0bac671
Ryan Galletto <ryan.galletto@netfoundry.io>
2024-08-28 13:10:46 -0400

    Reduce application bundle size using production configuration

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 npm install
 npm install -g @angular/cli@16.0.0-next.0
 ng build ziti-console-lib
-ng build ziti-console --base-href $1 --deploy-url $1
+ng build ziti-console --base-href $1 --deploy-url $1 --configuration "production"
