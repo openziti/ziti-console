e097ad9438529f4509ba9daf5ee67babfe1a6027
Mohammed Omer <info@mohammedomer.vip>
2024-12-20 10:54:52 +0530

    Refactor public-api.ts to group exports and improve readability
    This restructuring improves the maintainability of the file, making it easier to navigate through the various components, services, pages, and models.

--- projects/ziti-console-lib/src/public-api.ts
+++ projects/ziti-console-lib/src/public-api.ts
@@ -2,12 +2,58 @@
  * Public API Surface of ziti-console-lib
  */
 
+// Modules
 export * from './lib/ziti-console-lib.module';
 export * from './lib/zac-routing.module';
+
+// Features
+export * from './lib/features/extendable/extendable.component';
+export * from './lib/features/sidebars/side-banner/side-banner.component';
+export * from './lib/features/sidebars/side-navbar/side-navbar.component';
+export * from './lib/features/sidebars/side-toolbar/side-toolbar.component';
+export * from './lib/features/projectable-forms/configuration/configuration-form.component';
+export * from './lib/features/projectable-forms/identity/identity-form.component';
+export * from './lib/features/projectable-forms/edge-router/edge-router-form.component';
+export * from './lib/features/projectable-forms/service/service-form.component';
+export * from './lib/features/projectable-forms/service/simple-service/simple-service.component';
+export * from './lib/features/projectable-forms/service-policy/service-policy-form.component';
+export * from './lib/features/projectable-forms/edge-router-policy/edge-router-policy-form.component';
+export * from './lib/features/projectable-forms/service-edge-router-policy/service-edge-router-policy-form.component';
+export * from './lib/features/projectable-forms/terminator/terminator-form.component';
+export * from './lib/features/projectable-forms/jwt-signer/jwt-signer-form.component';
+export * from './lib/features/projectable-forms/auth-policy/auth-policy-form.component';
+export * from './lib/features/projectable-forms/form-header/form-header.component';
+export * from './lib/features/projectable-forms/form-field-container/form-field-container.component';
+export * from './lib/features/projectable-forms/form-field-toggle/form-field-toggle.component';
+
+// Services
 export * from './lib/features/wrappers/zac-wrapper.component';
 export * from './lib/features/wrappers/zac-wrapper.service';
 export * from './lib/features/wrappers/zac-wrapper-service.class';
 export * from './lib/features/wrappers/node-wrapper.service';
+export * from './lib/features/messaging/growler.component';
+export * from './lib/features/messaging/growler.service';
+export * from './lib/features/messaging/logger.service';
+export * from './lib/features/messaging/growler.model';
+export * from './lib/features/data-table/data-table-filter.service';
+export * from './lib/features/side-modal/side-modal.component';
+export * from './lib/services/login-service.class';
+export * from './lib/services/noop-login.service';
+export * from './lib/services/settings-service.class';
+export * from './lib/services/settings.service';
+export * from './lib/services/tab-name.service';
+export * from './lib/services/ziti-data.service';
+export * from './lib/services/node-data.service';
+export * from './lib/services/ziti-controller-data.service';
+export * from './lib/services/ziti-domain-controller.service';
+export * from './lib/services/deactivate-guard.service';
+export * from './lib/services/csv-download.service';
+export * from './lib/features/projectable-forms/configuration/configuration.service';
+export * from './lib/features/extendable/extensions-noop.service';
+export * from './lib/features/reset-enrollment/reset-enrollment.service';
+export * from './lib/features/visualizer/visualizer-service.class';
+
+// Pages
 export * from './lib/pages/identities/identities-page.component';
 export * from './lib/pages/identities/identities-page.service';
 export * from './lib/pages/edge-routers/edge-routers-page.component';
@@ -24,75 +70,18 @@ export * from './lib/pages/jwt-signers/jwt-signers-page.component';
 export * from './lib/pages/jwt-signers/jwt-signers-page.service';
 export * from './lib/pages/auth-policies/auth-policies-page.component';
 export * from './lib/pages/auth-policies/auth-policies-page.service';
-export * from './lib/services/login-service.class';
-export * from './lib/services/noop-login.service';
-export * from './lib/services/settings-service.class';
-export * from './lib/services/settings.service';
-export * from './lib/services/tab-name.service';
-export * from './lib/services/ziti-data.service';
-export * from './lib/services/node-data.service';
-export * from './lib/services/ziti-controller-data.service';
-export * from './lib/services/ziti-domain-controller.service';
-export * from './lib/services/deactivate-guard.service';
-export * from './lib/services/csv-download.service';
-export * from './lib/features/projectable-forms/configuration/configuration.service';
-export * from './lib/features/projectable-forms/configuration/configuration-form.component';
-export * from './lib/features/extendable/extendable.component';
-export * from './lib/features/data-table/data-table-filter.service';
-export * from './lib/features/card-list/card-list.component';
-export * from './lib/features/sidebars/side-toolbar/side-toolbar.component';
-export * from './lib/features/sidebars/side-navbar/side-navbar.component';
-export * from './lib/features/sidebars/side-banner/side-banner.component';
-export * from './lib/features/dynamic-widgets/string/string-input.component';
-export * from './lib/features/dynamic-widgets/number/number-input.component';
-export * from './lib/features/dynamic-widgets/selector/selector-input.component';
-export * from './lib/features/dynamic-widgets/password/password-input.component';
-export * from './lib/features/dynamic-widgets/boolean/boolean-toggle-input.component';
-export * from './lib/features/dynamic-widgets/checkbox-list/checkbox-list-input.component';
-export * from './lib/features/dynamic-widgets/object/object.component';
-export * from './lib/features/dynamic-widgets/text-list/text-list-input.component';
-export * from './lib/features/dynamic-widgets/protocol-address-port/protocol-address-port-input.component';
-export * from './lib/features/projectable-forms/projectable-form.class';
-export * from './lib/features/messaging/growler.component';
-export * from './lib/features/messaging/growler.service';
-export * from './lib/features/messaging/growler.model';
-export * from './lib/features/messaging/logger.service';
 export * from './lib/pages/configurations/configurations-page.component';
 export * from './lib/pages/terminators/terminators-page.component';
 export * from './lib/pages/terminators/terminators-page.service';
-export * from './lib/ziti-console.constants';
-export * from './lib/features/side-modal/side-modal.component';
-export * from './lib/features/projectable-forms/identity/identity-form.component';
-export * from './lib/features/projectable-forms/identity/identity-form.service';
-export * from './lib/features/projectable-forms/edge-router/edge-router-form.component';
-export * from './lib/features/projectable-forms/edge-router/edge-router-form.service';
-export * from './lib/features/projectable-forms/service/service-form.component';
-export * from './lib/features/projectable-forms/service/service-form.service';
-export * from './lib/features/projectable-forms/service/simple-service/simple-service.component';
-export * from './lib/features/projectable-forms/service-policy/service-policy-form.service';
-export * from './lib/features/projectable-forms/service-policy/service-policy-form.component';
-export * from './lib/features/projectable-forms/edge-router-policy/edge-router-policy-form.service';
-export * from './lib/features/projectable-forms/edge-router-policy/edge-router-policy-form.component';
-export * from './lib/features/projectable-forms/service-edge-router-policy/service-edge-router-policy-form.service';
-export * from './lib/features/projectable-forms/service-edge-router-policy/service-edge-router-policy-form.component';
-export * from './lib/features/projectable-forms/terminator/terminator-form.service';
-export * from './lib/features/projectable-forms/terminator/terminator-form.component';
-export * from './lib/features/projectable-forms/jwt-signer/jwt-signer-form.service';
-export * from './lib/features/projectable-forms/jwt-signer/jwt-signer-form.component';
-export * from './lib/features/projectable-forms/auth-policy/auth-policy-form.service';
-export * from './lib/features/projectable-forms/auth-policy/auth-policy-form.component';
-export * from './lib/features/reset-enrollment/reset-enrollment.service';
-export * from './lib/features/extendable/extensions-noop.service';
-export * from './lib/features/projectable-forms/form-header/form-header.component';
-export * from './lib/features/loading-indicator/loading-indicator.component';
+
+// Models
 export * from './lib/models/identity';
 export * from './lib/models/edge-router';
-export * from './lib/features/messaging/growler.module';
-export * from './lib/shared/list-page-component.class';
-export * from './lib/shared/list-page-service.class';
-export * from './lib/features/projectable-forms/form-field-container/form-field-container.component';
-export * from './lib/features/projectable-forms/form-field-toggle/form-field-toggle.component';
+
+// Constants
+export * from './lib/ziti-console.constants';
 export * from './lib/version';
-export * from './lib/features/visualizer/visualizer-service.class';
+
+// Visualizer
 export * from './lib/features/visualizer/identity-service-path/identity-service-path.component';
 export * from './lib/features/visualizer/network-visualizer/network-visualizer.component';
