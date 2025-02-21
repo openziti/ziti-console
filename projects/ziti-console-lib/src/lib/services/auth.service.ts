2b92e1e735f102157d5b188d302c8e7695f846a8
Ryan Galletto <ryan.galletto@netfoundry.io>
2025-02-21 11:22:01 -0500

    * Correct "Log in" vs "Login" vs "Sign in" * Fix hardcoded callback URL

--- projects/ziti-console-lib/src/lib/services/auth.service.ts
+++ projects/ziti-console-lib/src/lib/services/auth.service.ts
@@ -1,23 +1,38 @@
-import { Injectable } from '@angular/core';
+import {Inject, Injectable} from '@angular/core';
 import { OAuthService } from 'angular-oauth2-oidc';
 import { HttpClient } from '@angular/common/http';
 import {GrowlerModel} from "../features/messaging/growler.model";
 import {GrowlerService} from "../features/messaging/growler.service";
+import {APP_BASE_HREF} from "@angular/common";
+import {NavigationEnd, Router} from "@angular/router";
 
 @Injectable({
     providedIn: 'root'
 })
 export class AuthService {
+    basePath = '/';
     constructor(
         private oauthService: OAuthService, private http: HttpClient,
         private growlerService: GrowlerService,
+        private router: Router
     ) {
+        router.events.subscribe((event: any) => {
+            if (event => event instanceof NavigationEnd) {
+                if (!event?.snapshot?.routeConfig?.path) {
+                    return;
+                }
+                const pathSegments = event.snapshot.routeConfig.path.split('/');
+                this.basePath = pathSegments[0];
+            }
+        });
     }
 
     public configureOAuth(issuer, clientId, audience, scopes?): Promise<any> {
+        let basePath = document.querySelector('base')?.getAttribute('href') || '/';
+        basePath = basePath.endsWith('/') ? basePath.slice(0, -1) : basePath;
         const oauthConfig = {
             issuer: issuer,
-            redirectUri: window.location.origin + '/zac/callback',
+            redirectUri: window.location.origin + `${basePath}/callback`,
             clientId: clientId,
             responseType: 'code',
             scope: scopes,
