363b4bafbc45418c073d507cb723afc32accde88
Ryan Galletto <ryan.galletto@netfoundry.io>
2026-01-05 16:09:01 -0500

    added missing version file refactor

--- version.js
+++ version.js
@@ -10,8 +10,8 @@ const packageJsonRaw = fs.readFileSync("./package.json", 'utf8');
 const packageJson = JSON.parse(packageJsonRaw);
 const zacVersion = packageJson.version;
 
-const file = path.resolve(__dirname, 'projects/ziti-console-lib/src/lib', 'version.ts');
-fsExtra.writeFileSync(file,`export const VERSION = {
+const file = path.resolve(__dirname, 'projects/ziti-console-lib/src/lib', 'ZAC_VERSION.ts');
+fsExtra.writeFileSync(file,`export const ZAC_VERSION = {
     "version": "${zacVersion}"
 };
 `, { encoding: 'utf-8' });
