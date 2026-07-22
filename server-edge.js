e4345c000e431b01af594c364d83433f5882f1be
Ryan Galletto <ryan.galletto@netfoundry.io>
2026-07-22 14:52:57 -0400

    Remove all references to google tag manager

--- server-edge.js
+++ server-edge.js
@@ -33,13 +33,13 @@ var corsOptions = {
 var helmetOptions = {
     contentSecurityPolicy: {
       directives: {
-        styleSrc: ["'self'", 'www.googletagmanager.com', 'www.google-analytics.com', 'openstreetmap.org', "'unsafe-inline'"],
-        scriptSrc: ["'self'", 'www.googletagmanager.com', 'www.google-analytics.com', 'openstreetmap.org', "'unsafe-inline'", "'unsafe-eval'"],
-        scriptSrcAttr: ["'self'", 'www.googletagmanager.com', 'www.google-analytics.com', 'openstreetmap.org', "'unsafe-inline'", "'unsafe-eval'"],
-        imgSrc: ["'self'", 'www.googletagmanager.com', 'www.google-analytics.com', 'openstreetmap.org', 'b.tile.opernstreetmap.org', 'data:', 'blob:', 'https:'],
-        frameSrc: ["'self'", 'www.googletagmanager.com', 'openstreetmap.org'],
-        frameAncestors: ["'self'", 'www.googletagmanager.com', 'openstreetmap.org'],
-        mediaSrc: ["'self'", 'www.googletagmanager.com', 'openstreetmap.org', 'data:', 'blob:', 'https:'],
+        styleSrc: ["'self'", 'openstreetmap.org', "'unsafe-inline'"],
+        scriptSrc: ["'self'", 'openstreetmap.org', "'unsafe-inline'", "'unsafe-eval'"],
+        scriptSrcAttr: ["'self'", 'openstreetmap.org', "'unsafe-inline'", "'unsafe-eval'"],
+        imgSrc: ["'self'", 'openstreetmap.org', 'b.tile.opernstreetmap.org', 'data:', 'blob:', 'https:'],
+        frameSrc: ["'self'", 'openstreetmap.org'],
+        frameAncestors: ["'self'", 'openstreetmap.org'],
+        mediaSrc: ["'self'", 'openstreetmap.org', 'data:', 'blob:', 'https:'],
         connectSrc: ["'self'", '*'],
       },
     },
