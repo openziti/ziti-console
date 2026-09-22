4aac513bfa495d1c89cfff26acd0efe04d550d9d
Vicky <vicky.pachori@netfoundry.io>
2026-09-22 18:54:11 +0530

    Exclude 'badges' from API submission in transit-router-form
    Removed 'badges' from the properties sent to the API in mergeUnknownFormProperties.

--- projects/ziti-console-lib/src/lib/features/projectable-forms/transit-router/transit-router-form.service.ts
+++ projects/ziti-console-lib/src/lib/features/projectable-forms/transit-router/transit-router-form.service.ts
@@ -72,8 +72,7 @@ export class TransitRouterFormService {
                     saveModel[prop] = formData[prop];
             }
         });
-        // `badges` is a UI-only property added directly onto formData and must not be sent to the API.
-        return this.zitiService.mergeUnknownFormProperties(saveModel, formData, modelProperties, ['id', 'badges']);
+        return this.zitiService.mergeUnknownFormProperties(saveModel, formData, modelProperties, ['id']);
     }
 
     getAuthPolicies() {
